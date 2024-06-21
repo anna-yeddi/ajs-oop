9cb55535e285f3d819b66f242739b6dd5c8c4096
Anna Kova <m3ta007@gmail.com>
2024-06-20 20:34:05 -0400

    Improving readability of Character constructor

--- src/js/characters/character.js
+++ src/js/characters/character.js
@@ -5,20 +5,25 @@ export default class Character {
     if (typeof name !== "string") {
       throw new Error("The name should a string");
     }
+
     if (name.length < 2) {
       throw new Error("The name should be at least 2 characters long");
-    } else if (name.length > 10) {
+    }
+
+    if (name.length > 10) {
       throw new Error("The name should be no more than 10 characters long");
     }
-    this.name = name;
+
     if (!types.includes(type)) {
       throw new Error("Unknown character type");
-    } else {
-      this.type = type;
     }
+
+    this.name = name;
+    this.type = type;
 
     this.health = 100;
     this.level = 1;
+
     this.attack = null;
     this.defense = null;
   }
